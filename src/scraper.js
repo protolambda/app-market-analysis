1e0d5790e7574e458fb8e6e48d3f801a03d03521
protolambda <proto@protolambda.com>
2018-10-14 15:31:15 +0800

    Change fs library to use promise based API, and fix output formatting

--- src/scraper.js
+++ src/scraper.js
@@ -1,27 +1,43 @@
 import gplay from "google-play-scraper";
-import fs from "fs";
+import fs from "babel-fs";
 
+function escapeSpecialChars(str) {
+    return str
+        .replace(/[\\]/g, '\\\\')
+        .replace(/[\"]/g, '\\\"')
+        .replace(/[\/]/g, '\\/')
+        .replace(/[\b]/g, '\\b')
+        .replace(/[\f]/g, '\\f')
+        .replace(/[\n]/g, '\\n')
+        .replace(/[\r]/g, '\\r')
+        .replace(/[\t]/g, '\\t');
+}
 
-async function scrapePage(pageNum) {
+const strF = (v) => "\""+escapeSpecialChars(v.toString())+"\"";
+
+function formatEntry(entry) {
+    // We want to save the date as a UNIX timestamp
+    const time = Date.parse(entry.date + ' GMT');
+
+    // Select the data we want to save
+    // Note: we save the ID too, so we can detect duplicate entries when verifying the data.
+    const dataRow = [time, strF(entry.userName), entry.score, strF(entry.title), strF(entry.text), strF(entry.id)];
+
+    return dataRow.join(",")
+}
+
+async function scrapePage(appID, pageNum, resultsFile) {
     // Sorting options: sort.NEWEST, sort.RATING, sort.HELPFULNESS
     // Use "NEWEST", no bias, most recent information. Great!
-    const result = gplay.reviews({
+    const result = await gplay.reviews({
         appId: appID,
         page: pageNum,
         sort: gplay.sort.NEWEST
     });
 
-    // We want to save the date as a UNIX timestamp
-    const time = Date.parse(result.date + ' GMT');
-
-    // Select the data we want to save
-    // Note: we save the ID too, so we can detect duplicate entries when verifying the data.
-    const dataRow = [time, result.userName, result.score, result.title, result.text, result.id];
 
     // Append the row to the results file (CSV)
-    await fs.appendFile(reviewScraper.resultsFile,
-        dataRow.map((v) => JSON.stringify(v)).join(",")
-    );
+    await fs.appendFile(resultsFile, result.map(formatEntry).join("\n"));
 }
 
 /**
@@ -30,21 +46,30 @@ async function scrapePage(pageNum) {
  * @param reviewScraper The scraper configuration.
  * @return {Promise<void>}
  */
-export async function startScraping({appID, reviewScraper}) {
+export async function startScraping(appID, reviewScraper) {
+
+    try {
+        if (!(fs.existsSync(reviewScraper.progressFile))) await fs.writeFile(reviewScraper.progressFile, "0").catch((v) => console.log("3", v));
+        if (!(fs.existsSync(reviewScraper.resultsFile))) await fs.writeFile(reviewScraper.resultsFile, "").catch((v) => console.log("4", v));
+    } catch (err) {
+        console.log("Could not create progress/result files for scraping!", err);
+        return;
+    }
+
     let retries = 0;
     while(true) {
         // Read where we stopped scraping
-        const pageNum = parseInt((await fs.readFile(reviewScraper.progressFile)) || "0");
-        if (pageNum >= reviewScraper.pageCount) {
+        const prevPageNum = parseInt((await fs.readFile(reviewScraper.progressFile).catch(console.log)) || "0");
+        if (prevPageNum >= reviewScraper.pageCount) {
             console.log("Finished scraping");
             break;
         }
-        const nextPageNum = pageNum + 1;
+        const currentPageNum = prevPageNum + 1;
         try {
             // Scrape the next page
-            await scrapePage(nextPageNum);
+            await scrapePage(appID, currentPageNum, reviewScraper.resultsFile);
         } catch (err) {
-            console.log("Failed to scrape page! Page number: ", nextPageNum, " error: ", err);
+            console.log("Failed to scrape page! Page number: ", currentPageNum, " error: ", err);
             retries++;
             if (retries > 3) {
                 console.log("Retried scraping 3 times! Stopping!");
@@ -55,7 +80,7 @@ export async function startScraping({appID, reviewScraper}) {
             continue;
         }
         // Save our progress
-        await fs.writeFile(reviewScraper.progressFile, nextPageNum.toString());
+        await fs.writeFile(reviewScraper.progressFile, currentPageNum.toString()).catch(console.log);
         // Sleep 0.1 seconds, we do not want the Google API to stop responding.
         await sleep(100);
     }
